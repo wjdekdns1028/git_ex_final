b866b0c61bd60cdca68a25dcefb9a30899e3cc12
Jung, Daun <wjdekdns0218@gmail.com>
2025-04-02 09:57:48 +0900

    fifth

--- src/World.java
+++ src/World.java
@@ -1,8 +1,11 @@
 public class World {
 
     public static void run() {
-      System.out.println("202212061");
+        System.out.println("202212061");
     }
-  
-  }
-  
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
+
+}
